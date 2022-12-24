bb70d0698748d188a8a77e57da99aba0976b90c2
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-12-24 12:54:30 -0500

    base project upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Cashvelope
+
+Digital cash stuffing tracker
+
+**Link to project:** coming soon
+
+## How It's Made:
+
+**Tech used:** React, Vite, JavaScript
+
+## Optimizations
+
+TBD
+
+## Lessons Learned:
+
+TBD
---
+++ src/components/Balance.jsx
@@ -0,0 +1,18 @@
+import React, {useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const Balance = () => {
+
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((sum, curr) => sum + curr, 0).toFixed(2);
+
+  return (
+    <>
+        <h4>Your Balance:</h4>
+        <h1>${total}</h1>
+    </>
+  )
+}
